e857e9aba75e40745b2c96933fa696144f87c194
TroddenSpade <parsasam1378@gmail.com>
2019-05-23 15:02:27 +0430

    4player Field added to switch navigator

--- App.js
+++ App.js
@@ -6,12 +6,14 @@ import StartScreen from './src/components/StartScreen';
 import Lobby from './src/components/Lobby';
 import Field2 from './src/components/Fields/Field2';
 import Field3 from './src/components/Fields/Field3';
+import Field4 from './src/components/Fields/Field4';
 
 const AppNavigator = createSwitchNavigator({
   StartScreen: StartScreen,
   Lobby: Lobby,
   Field2: Field2,
   Field3: Field3,
+  Field4: Field4,
 },{
   initialRouteName : "StartScreen"
 });
